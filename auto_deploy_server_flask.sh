03c4c2b09c774ed6bbdb61c814daa9fc18d70cbc
dev-jackson <angeljackson0933@gmail.com>
2021-06-04 14:29:02 -0500

    Indentify platform and exist pacakes

--- auto_deploy_server_flask.sh
+++ auto_deploy_server_flask.sh
@@ -1,10 +1,44 @@
 #!bin/bash
 
 printf "Cheking dependecies...\n"
+platform='unknown'
+command_install='unknown'
 
-dependecies=("python3" "pip")
+# Verify sistem adn unpdate packages
+if [ -f /etc/arch-release ] ; then 
+    platform="Arch-linux"
+    command_install="pacman"
+    sudo "${command_install}" -Sy
+elif [ -f /etc/redhat-release ] ; then 
+    platform="Red Hat"
+    command_install="yum"
+    sudo "${command_install}" update
+elif [ -f /etc/fedora-release ] ; then
+    platform="Fedora"
+    command_install="dnf"
+    sudo "${command_install}" update
+elif [ -f /etc/debian_release ] ; then
+    platform="Debian"
+    command_install="apt-get"
+    sudo "${command_install}" update
+fi 
 
-for i in "${dependecies[@]}"
-do 
-    echo "Type ${i}"
-done 
+echo "Platform => " "${platform}"
+
+dependecies_bash=("python3" "pip") 
+packages_python=("virtualenv" "Flask" "unicorn")
+
+for i in "${dependecies_bash[@]}"
+    do 
+        if ! command -v "${i}" &> /dev/null ; then
+            printf "Not found to => "
+            if [ "${command_install}" == "pacman" ] ; then
+                sudo "${command_install}" -S "${i}" 
+            else 
+                sudo "${command_install}" install "${i}"
+            fi
+
+        else
+             "${i}" --version
+        fi
+    done 
